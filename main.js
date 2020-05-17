6572d6458098a04fcb44c1e9f3545047dc9d2619
Lyndsey M <lyndsey@lyndseyjane.com>
2020-05-17 13:32:24 -0400

    doll maker

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-Vue.component('todo-item', {
-    template: '<img :src={{subtodo.txt}}>',
-    props: ['subtodo']
-  })
-  
-  var app = new Vue({
+var app = new Vue({
     el: '#app',
     data: function () {
         return {
         todos: [
-          { text : 'hair', 
+          { text : 'Hair', 
               subTodos : [{ text : 'doll/hair1.png'}, { text : 'doll/hair2.png'}, { text : 'doll/hair3.png'}, { text : 'doll/hair4.png'}]
           },
-          { text : 'shirts', 
-              subTodos : [{ text : 'shirt'}, { text : 't-shirt'}, { text : 'shirt'}]
+          { text : 'Shirts', 
+              subTodos : [{ text : 'doll/shirt1.png'}, { text : 'doll/shirt2.png'}, { text : 'doll/shirt3.png'}]
           },
-          { text : 'maybe accessories here', 
+          { text : 'Pants', 
+              subTodos : [{ text : 'doll/pants1.png'}, { text : 'doll/pants2.png'}, { text : 'doll/pants3.png'}]
+        },
+          { text : 'Accessories', 
               subTodos : [{ text : 'hat'}, { text : 'accessory'}, { text : 'accessory'}]
-          }
+          },
         ]
         }
     }
