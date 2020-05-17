3ab29ac0dc16efd69905b06812424724293f9dee
Lyndsey M <lyndsey@lyndseyjane.com>
2020-05-17 11:51:05 -0400

    I forgot how Vue works

---
+++ main.js
@@ -0,0 +1,23 @@
+Vue.component('todo-item', {
+    template: '<li>{{subtodo.text}}</li>',
+    props: ['subtodo']
+  })
+  
+  var app = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+        todos: [
+          { text : 'hair', 
+              subTodos : [{ text : 'is this working'}, { text : 'why am i developing on windows'}, { text : 'Testing'}]
+          },
+          { text : 'shirts', 
+              subTodos : [{ text : 'shirt'}, { text : 't-shirt'}, { text : 'shirt'}]
+          },
+          { text : 'maybe accessories here', 
+              subTodos : [{ text : 'hat'}, { text : 'accessory'}, { text : 'accessory'}]
+          }
+        ]
+        }
+    }
+  })
