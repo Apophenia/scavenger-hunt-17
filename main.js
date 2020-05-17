292a02e80ec65392f27a7805164e8a9b76d401a7
Lyndsey M <lyndsey@lyndseyjane.com>
2020-05-17 13:21:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 Vue.component('todo-item', {
-    template: '<li>{{subtodo.text}}</li>',
+    template: '<img :src={{subtodo.txt}}>',
     props: ['subtodo']
   })
   
@@ -9,7 +9,7 @@ Vue.component('todo-item', {
         return {
         todos: [
           { text : 'hair', 
-              subTodos : [{ text : 'hair1.png here'}, { text : 'why am i developing on windows'}, { text : 'Testing'}]
+              subTodos : [{ text : 'doll/hair1.png'}, { text : 'doll/hair2.png'}, { text : 'doll/hair3.png'}, { text : 'doll/hair4.png'}]
           },
           { text : 'shirts', 
               subTodos : [{ text : 'shirt'}, { text : 't-shirt'}, { text : 'shirt'}]
